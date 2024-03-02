abad4e402acd30e9dec5104ae7ffff3ad8fca2ac
Dmitry <white-wolfs@mail.ru>
2024-03-02 15:37:27 +0300

    Module17

---
+++ Module17/Module17/Module17/Module17.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <math.h>
+
+
+class Vector
+{
+private:
+	int x = 3;
+	int y = 4;
+
+public:
+	Vector() {
+		std::cout << "Vector = " << std::sqrt(std::pow(x, 2) + std::pow(y, 2)) << std::endl;
+	}
+	Vector( int newX, int newY) {
+		std::cout << "Vector = " << std::sqrt(std::pow(newX, 2) + std::pow(newY, 2)) << std::endl;
+	}
+	void SetVector( int newX, int newY) {
+		std::cout << "Vector = " << std::sqrt(std::pow(newX, 2) + std::pow(newY, 2)) << std::endl;
+	}
+};
+
+
+int main()
+{
+	Vector vector;
+	vector.SetVector(2,3);
+	Vector::Vector(2, 4);
+	return 0;
+}
