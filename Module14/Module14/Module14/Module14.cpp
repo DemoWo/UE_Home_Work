7a78344e34fd4eec84f146c3ca2e91aed0df4641
Dmitry <white-wolfs@mail.ru>
2024-03-02 10:29:05 +0300

    Module 14

---
+++ Module14/Module14/Module14/Module14.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::string Word;
+    Word = "Lesson Unreal Engine";
+    std::cout << "Word: " << Word << ", Length: " << Word.length() << std::endl;
+    std::cout << "SymOne: " << Word[0] << ", SymEnd: " << Word[Word.length() - 1] << std::endl;
+}
