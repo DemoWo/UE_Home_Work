dfa95ff5bf9b56f422168b797df1f33a548121bf
Dmitry <white-wolfs@mail.ru>
2024-03-02 12:23:32 +0300

    Module15

---
+++ Module15/Module15/Module15/Module15.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+void FindOddNumbers(int Limit, bool IsOdd)
+{
+    for (int i = 0 ; i  < Limit; ++i )
+    {
+        ( IsOdd && !(i % 2) || !IsOdd && (i % 2) ) && std::cout << i << std::endl;
+    }
+}
+
+int main()
+{
+    FindOddNumbers(10, true);
+}
+
